efc0c8bc696c9b9327c8bcdabd803069b0ba32d0
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-07 11:24:58 -0500

    Update entryway.cpp

--- modules/entryway/entryway.cpp
+++ modules/entryway/entryway.cpp
@@ -4,6 +4,18 @@
 #include "ldr_sensor.h"
 #include "smart_home_system.h"
 
+DigitalOut eLED(PE_10);
+
 void entrywayLEDon();
+{
+  if () {
+    eLED = ON;
+  }
+}
 
 void entrywayLEDoff();
+{
+  if () {
+    eLED = OFF;
+  }
+}
