e99604ba719e092447a80f9679944b4fa5f52c4f
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-07 12:50:42 -0500

    Update rgb.cpp

--- modules/rgb/rgb.cpp
+++ modules/rgb/rgb.cpp
@@ -4,4 +4,46 @@ void toGreenRGBLED()
 
 void toRedRGBLED()
 {
+    setPeriod( RGB_LED_RED, 1 );
+    setPeriod( RGB_LED_GREEN, 0 );
+    setPeriod( RGB_LED_BLUE, 0 );
+
+    setDutyCycle( RGB_LED_RED, 1 );
+    setDutyCycle( RGB_LED_GREEN, 0 );
+    setDutyCycle( RGB_LED_BLUE, 0 );
 }
+
+
+
+#include "arm_book_lib.h"
+
+
+
+#define LEDS_QUANTITY 3
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+DigitalOut RGBLed[] = {(PB_4), (PA_0), (PD_12)};
+
+
+
+static void setPeriod( lightSystem_t light, float period );
+
+//=====[Implementations of public functions]===================================
+
+void brightControlInit()
+{
+    tickerBrightControl.attach( tickerCallbackBrightControl, 
+                              ( (float) tickRateMSBrightControl) / 1000.0 );
+
+    setPeriod( RGB_LED_RED, 0.01f );
+    setPeriod( RGB_LED_GREEN, 0.01f );
+    setPeriod( RGB_LED_BLUE, 0.01f );
+
+    setDutyCycle( RGB_LED_RED, 0.5f );
+    setDutyCycle( RGB_LED_GREEN, 0.5f );
+    setDutyCycle( RGB_LED_BLUE, 0.5f );
+}
+
