dcca8d9fe01968d48adb98f726da840abe86a177
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-07 15:46:35 -0500

    Update rgb.cpp

--- modules/rgb/rgb.cpp
+++ modules/rgb/rgb.cpp
@@ -1,29 +1,21 @@
 #include "arm_book_lib.h"
 
-
-
-DigitalOut RGBLed[] = {(PB_4), (PA_0), (PD_12)};
-
+DigitalOut r(PA_0);
+DigitalOut g(PD_12);
+DigitalOut b(PB_4);
+DigitalOut rgbLED[] = {r, b, g};
 
 
 void toGreenRGBLED()
 {
-    setPeriod( RGB_LED_RED, 0 );
-    setPeriod( RGB_LED_GREEN, 1 );
-    setPeriod( RGB_LED_BLUE, 0 );
-
-    setDutyCycle( RGB_LED_RED, 0 );
-    setDutyCycle( RGB_LED_GREEN, 1 );
-    setDutyCycle( RGB_LED_BLUE, 0 );
+    r = 0.0;
+    g = 1.0;
+    b = 0.0;
 }
 
 void toRedRGBLED()
 {
-    setPeriod( RGB_LED_RED, 1 );
-    setPeriod( RGB_LED_GREEN, 0 );
-    setPeriod( RGB_LED_BLUE, 0 );
-
-    setDutyCycle( RGB_LED_RED, 1 );
-    setDutyCycle( RGB_LED_GREEN, 0 );
-    setDutyCycle( RGB_LED_BLUE, 0 );
+    r = 1.0;
+    g = 0.0;
+    b = 0.0;
 }
