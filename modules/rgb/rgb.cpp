496d68e0e61f0326b6aaf06be1e83e6f1d7991d9
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-07 13:30:33 -0500

    Update rgb.cpp

--- modules/rgb/rgb.cpp
+++ modules/rgb/rgb.cpp
@@ -1,5 +1,20 @@
+#include "arm_book_lib.h"
+
+
+
+DigitalOut RGBLed[] = {(PB_4), (PA_0), (PD_12)};
+
+
+
 void toGreenRGBLED()
 {
+    setPeriod( RGB_LED_RED, 0 );
+    setPeriod( RGB_LED_GREEN, 1 );
+    setPeriod( RGB_LED_BLUE, 0 );
+
+    setDutyCycle( RGB_LED_RED, 0 );
+    setDutyCycle( RGB_LED_GREEN, 1 );
+    setDutyCycle( RGB_LED_BLUE, 0 );
 }
 
 void toRedRGBLED()
@@ -12,38 +27,3 @@ void toRedRGBLED()
     setDutyCycle( RGB_LED_GREEN, 0 );
     setDutyCycle( RGB_LED_BLUE, 0 );
 }
-
-
-
-#include "arm_book_lib.h"
-
-
-
-#define LEDS_QUANTITY 3
-
-//=====[Declaration of private data types]=====================================
-
-//=====[Declaration and initialization of public global objects]===============
-
-DigitalOut RGBLed[] = {(PB_4), (PA_0), (PD_12)};
-
-
-
-static void setPeriod( lightSystem_t light, float period );
-
-//=====[Implementations of public functions]===================================
-
-void brightControlInit()
-{
-    tickerBrightControl.attach( tickerCallbackBrightControl, 
-                              ( (float) tickRateMSBrightControl) / 1000.0 );
-
-    setPeriod( RGB_LED_RED, 0.01f );
-    setPeriod( RGB_LED_GREEN, 0.01f );
-    setPeriod( RGB_LED_BLUE, 0.01f );
-
-    setDutyCycle( RGB_LED_RED, 0.5f );
-    setDutyCycle( RGB_LED_GREEN, 0.5f );
-    setDutyCycle( RGB_LED_BLUE, 0.5f );
-}
-
